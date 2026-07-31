b5f0dc749575c60ccadb89739939c8fa397ed6ff
Jason Galens <jason@galensfamily.org>
2026-07-31 07:52:41 -0400

    Add MILTER_DEFAULT to envsubst

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 echo "setting configuration"
-envsubst '$DOMAIN,$HOSTNAME,$TRANSPORT,$INCOMING_MILTERS,$OUTGOING_MILTERS' < /etc/postfix/main.cf.tpl > /etc/postfix/main.cf
+envsubst '$DOMAIN,$HOSTNAME,$TRANSPORT,$MILTER_DEFAULT,$INCOMING_MILTERS,$OUTGOING_MILTERS' < /etc/postfix/main.cf.tpl > /etc/postfix/main.cf
 
 echo "updating aliases"
 /usr/bin/newaliases
